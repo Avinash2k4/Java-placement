9c0e1fe15cab82e035f19e99414ea37bb281bcc6
Avinash2k4 <avinashilingam10@gmail.com>
2025-07-14 16:39:59 +0530

    Add files via upload

---
+++ Npalin.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Npalin {
+    public static Boolean ispalindrome(int num){
+        int og=num;int rev=0;
+        while(num>0){
+            int digit=num%10;
+            rev=rev*10+digit;
+            num/=10;
+        }
+        return og==rev;
+    }
+    public static int getNextpalin(int num){
+        num++;
+        while(!ispalindrome(num)){
+            num++;
+        }
+        return num;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int num=sc.nextInt();
+        int Result=getNextpalin(num);
+        System.out.println("Next palindrome:"+Result);
+    }
+}
